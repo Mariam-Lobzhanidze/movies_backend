90bb73c7056507dda106a8450eb3bfd8db034ef9
Mariam-Lobzhanidze <mariami.lobzhanidze7@gmail.com>
2024-11-22 16:56:30 +0400

    warm up server

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", routes);
 
+app.get("/api/health", (req, res) => {
+  res.status(200).send("Healthy");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
