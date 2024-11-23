a6f5adf3d1f2f4533e85a1993b44c6595be88ca6
Mariam-Lobzhanidze <mariami.lobzhanidze7@gmail.com>
2024-11-23 19:17:37 +0400

    removed vercel.json

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", routes);
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
