03bc81147e26ae5a388199d20366a2be643722b0
Mariam-Lobzhanidze <mariami.lobzhanidze7@gmail.com>
2024-11-23 18:09:46 +0400

    updated cors

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ require("./associations");
 
 const app = express();
 
-// app.use(cors());
-
 const corsOptions = {
-  origin: "https://moviesapp-ml.netlify.app/",
+  origin: "https://moviesapp-ml.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
+
+app.use(cors(corsOptions));
 
 app.use(cors(corsOptions));
 
