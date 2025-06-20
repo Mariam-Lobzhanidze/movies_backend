4ca7da57371a87a7bc84239a0b6ea837aeccd0bf
Mariam-Lobzhanidze <mariami.lobzhanidze7@gmail.com>
2025-06-20 21:28:59 +0400

    health check added

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", routes);
 
+app.get("/health", (req, res) => {
+  console.log(`[${new Date().toISOString()}] Ping received at /health`);
+  res.status(200).send("OK");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
